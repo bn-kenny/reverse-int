798256bbfb0200ca05aae64b77d2fd49fe8d6d3f
Ivan Bichan <bnkenny1987@gmail.com>
2021-03-15 19:07:48 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
+  if(!n){
+    return 0;
+  } else {
+    n = Math.abs(n);
+    let rev = 0;
+      while (n) {
+      rev = rev * 10 + n % 10;
+      n = Math.floor(n / 10);
+      } return rev;
+  }
   
 }
